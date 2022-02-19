d1f3b52f657b094b336f57009205eb6c5b0dd2a6
12Tubo <omoniighopat@gmail.com>
2022-02-19 15:20:19 +0000

    I am working on git landmark book

---
+++ dbconnect.java
@@ -0,0 +1,16 @@
+#!/bin/bash
+what is your name?
+read name
+how may I help you $name 
+echo " I need to make some withdrawals"
+echo "okay $name"
+echo "please enter your pin in the pad"
+read pin -s
+echo " your pin is correct"
+if $pin is 1234
+else
+echo " your pin is incorrect"
+echo "how much do you want?"
+read $amount
+echo "thank you Mr. $name"
+echo "do have a wonderful day Mr $name"
